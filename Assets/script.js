e13f10d5295e5191df4236878b8515380b1d4b9d
Nick <nicksweetj@gmail.com>
2023-05-05 00:22:36 -0400

    added format for quetions object, still need more questions

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,16 @@
 var startButton = document.getElementById("startButton")
 var timer = document.getElementById("timer")
-var timeLeft = 3;
+var timeLeft = 75;
+
+var questions = [{
+    id: 0,
+    question: "What event is not a valid option for addEventListener?",
+    answer: ["Click", "Keydown", "Scroll", "Jump"]
+},
+{
+    id: 1,
+}
+]
 
 startButton.addEventListener("click", function() {
     if (timeLeft > 0) {
