a6ed8a7666b91d63a4b00d81b8ebebfc7ff622c4
Nick <nicksweetj@gmail.com>
2023-05-08 17:23:05 -0400

    updated hiscores to sort through the array and list them highest to lowest

--- Assets/hiscore.js
+++ Assets/hiscore.js
@@ -1,6 +1,9 @@
 var scores = JSON.parse(localStorage.getItem("score")) || [];
 for (i = 0; i < scores.length; i++) {
     var li = document.createElement("li");
+    scores.sort((a,b) => {
+        return b.score - a.score;
+    });
     li.innerText = scores[i].name + " " + scores[i].score;
     document.getElementById("list").appendChild(li);
 }
