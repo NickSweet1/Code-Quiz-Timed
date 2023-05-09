b63bbc8c7d2079a4494364cd6333576dbcef43af
Nick <nicksweetj@gmail.com>
2023-05-08 23:57:55 -0400

    updated css background image to be more responsive

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,14 @@
-// interval = setInterval(function () {
-//   if (timeLeft > 0) {
-//     timer.textContent = "Time Left: " + timeLeft--;
-//   } else {
-//     clearInterval(interval);
-//     timer.textContent = "Out of Time!";
-//     var name = prompt("Type your name for the hiscores");
-//     var scores = JSON.parse(localStorage.getItem("score")) || [];
-//     scores.push({ name, score });
-//     localStorage.setItem("score", JSON.stringify(scores));
-//     window.location.replace(localURL);
-//     return;
-//   }
-// }, 1000);
+interval = setInterval(function () {
+  if (timeLeft > 0) {
+    timer.textContent = "Time Left: " + timeLeft--;
+  } else {
+    clearInterval(interval);
+    timer.textContent = "Out of Time!";
+    var name = prompt("Type your name for the hiscores");
+    var scores = JSON.parse(localStorage.getItem("score")) || [];
+    scores.push({ name, score });
+    localStorage.setItem("score", JSON.stringify(scores));
+    window.location.replace(localURL);
+    return;
+  }
+}, 1000);
